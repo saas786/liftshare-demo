7ce02f13c7a65f1b33081ed5130c6766c2b10545
Matthew Bolton <mattb_76@yahoo.co.uk>
2021-11-15 01:35:55 +0000

    upgrade vite; fix hmr using vite config;

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [vue()],
 });
